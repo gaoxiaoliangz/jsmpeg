701d3a835e347a371b12c2c3edf9b71fd726b1e6
Liang <gaoxiaoliangz@gmail.com>
2019-12-31 11:11:33 +0800

    chore: add build-js script

---
+++ build-js.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Concat all .js sources
+cat \
+	src/jsmpeg.js \
+	src/video-element.js \
+	src/player.js \
+	src/buffer.js \
+	src/ajax.js \
+	src/fetch.js \
+	src/ajax-progressive.js \
+	src/websocket.js \
+	src/ts.js \
+	src/decoder.js \
+	src/mpeg1.js \
+	src/mpeg1-wasm.js \
+	src/mp2.js \
+	src/mp2-wasm.js \
+	src/webgl.js \
+	src/canvas2d.js \
+	src/webaudio.js \
+	src/wasm-module.js \
+	> jsmpeg.js
+
+# Append the .wasm module to the .js source as base64 string
+echo "JSMpeg.WASM_BINARY_INLINED='$(cat jsmpeg-wasm.base64)';" \
+	>> jsmpeg.js
+
+cp jsmpeg.js example/jsmpeg.js
